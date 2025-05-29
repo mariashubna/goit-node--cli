860d25f5edf019375e08bc92d2f9ff2723fd11e5
mariashubna <mariashubna@gmail.com>
2025-05-29 19:50:03 +0200

    done

--- contacts.js
+++ contacts.js
@@ -5,21 +5,18 @@ const uuid = require("uuid");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
   const dataBuffer = await fs.readFile(contactsPath);
   const contactsList = JSON.parse(dataBuffer);
   return contactsList;
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
   const contactsList = await listContacts();
   const contact = contactsList.find((contact) => contact.id === contactId);
   return contact || null;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   const contactsList = await listContacts();
   const removeContact = await getContactById(contactId);
   if (removeContact) {
@@ -30,7 +27,6 @@ async function removeContact(contactId) {
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
   const id = uuid.v4();
   const newContact = { id, name, email, phone };
   const contactsList = await listContacts();
